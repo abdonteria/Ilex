79483691fac0a8c5d4f443652f7a86e434e231d6
insprya <insprya2gmail.com>
2025-03-11 17:33:26 +0200

    make pages and Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import Projects from "./pages/Projects";
+import SignUp from "./pages/SignUp";
+
 export default function App() {
-  return <div className="text-red-500">App</div>;
+  return (
+    <Routes>
+      <Route index element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/projects" element={<Projects />} />
+      <Route path="/signup" element={<SignUp />} />
+    </Routes>
+  );
 }
